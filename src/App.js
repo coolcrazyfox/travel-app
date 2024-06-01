e9dbed73cfdf8f2b8178eee69d55661c57d9d7cd
Andrei Malets <coolviktori@yandex.by>
2024-06-01 18:15:40 +0300

    changes  Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 import s from "./styles/Header.module.css";
 
 function App() {
   return (
     <div className={s.body}>
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaTelegram, FaTwitter } from "react-icons/fa";
+
+const iconsData = [
+  { title: "instagram", icon: <FaInstagram /> },
+  { title: "telegram", icon: <FaTelegram /> },
+  { title: "facebook", icon: <FaFacebook /> },
+  { title: "twitter", icon: <FaTwitter /> },
+];
+
+const Hero = () => {
+  return (
+    <section className="s.home">
+      <div className="s.content">
+        <h1>
+          Wonderful.
+          <br />
+          <span>Island</span>
+          <p>
+            lorem In cillum officia id nisi ut.Proident sint exercitation
+            consectetur nisi in anim laborum nulla consequat.Excepteur
+            reprehenderit sunt amet laborum nulla deserunt nulla reprehenderit
+            esse excepteur.Nisi duis enim id aliquip excepteur proident nulla
+            ullamco esse nisi quis magna duis.Ex dolore consequat voluptate
+            commodo commodo eiusmod duis laborum magna minim.Commodo ex anim
+            labore esse tempor nulla.Mollit duis cillum velit commodo velit enim
+            voluptate in ex esse aliqua anim excepteur tempor.Et adipisicing
+            culpa ex esse nostrud.Eu minim consectetur sunt anim consectetur
+            cupidatat et nisi amet.
+          </p>
+          <a href="#">Read More</a>
+        </h1>
+      </div>
+      <div className="s.media_icons">
+        {iconsData.map((item, index) => {
+          return (
+            <a key={item.title} className="s.link" href="#">
+              <i className="s.fab">{item.icon}</i>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
