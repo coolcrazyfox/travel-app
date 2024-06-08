baef913ed8780d77c12025956416a268dba91458
Andrei Malets <coolviktori@yandex.by>
2024-06-08 15:17:24 +0300

    changes Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,29 +17,24 @@ const Hero = () => {
           Wonderful.
           <br />
           <span className={s.span}>Island</span>
-          <p className={s.description}>
-            lorem In cillum officia id nisi ut.Proident sint exercitation
-            consectetur nisi in anim laborum nulla consequat.Excepteur
-            reprehenderit sunt amet laborum nulla deserunt nulla reprehenderit
-            esse excepteur.Nisi duis enim id aliquip excepteur proident nulla
-            ullamco esse nisi quis magna duis.Ex dolore consequat voluptate
-            commodo commodo eiusmod duis laborum magna minim.Commodo ex anim
-            labore esse tempor nulla.Mollit duis cillum velit commodo velit enim
-            voluptate in ex esse aliqua anim excepteur tempor.Et adipisicing
-            culpa ex esse nostrud.Eu minim consectetur sunt anim consectetur
-            cupidatat et nisi amet.
-          </p>
-          <a href="#" className={s.link_btn}>
-            Read More
-          </a>
         </h1>
+        <p className={s.description}>
+          lorem In cillum officia id nisi ut.Proident sint exercitation
+          consectetur nisi in anim laborum nulla consequat.Excepteur
+          reprehenderit sunt amet laborum nulla deserunt nulla reprehenderit
+          esse excepteur.Nisi duis enim id aliquip excepteur proident nulla
+          ullamco esse nisi quis magna duis.
+        </p>
+        <div href="#" className={s.link_btn}>
+          Read More
+        </div>
       </div>
       <div className={s.media_icons}>
         {iconsData.map((item, index) => {
           return (
-            <a key={item.title} className={s.link} href="#">
+            <div key={item.title} className={s.link} href="#">
               <i className={s.fab}>{item.icon}</i>
-            </a>
+            </div>
           );
         })}
       </div>
