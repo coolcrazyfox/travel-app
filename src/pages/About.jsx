11c00f5fc8d14345b1b5eb273d7fc9b73138c4e4
Andrei Malets <coolviktori@yandex.by>
2024-06-18 22:39:26 +0300

    changes About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import s from "../styles/Hero.module.css";
+import video from "../assets/about.mp4";
+import iconsData from "./Hero";
 const About = () => {
-  return <section className={s.home}>About</section>;
+  return (
+    <section className={s.home}>
+      {" "}
+      <video className={s.video_box} src={video} autoPlay muted loop></video>
+      <div className={s.content}>
+        <h1 className={s.h1}>
+          Wonderful.
+          <br />
+          <span className={s.span}>Island</span>
+        </h1>
+        <p className={s.description}>
+          lorem In cillum officia id nisi ut.Proident sint exercitation
+          consectetur nisi in anim laborum nulla consequat.Excepteur
+          reprehenderit sunt amet laborum nulla deserunt nulla reprehenderit
+          esse excepteur.Nisi duis enim id aliquip excepteur proident nulla
+          ullamco esse nisi quis magna duis.
+        </p>
+        <a href="link" className={s.link_btn}>
+          Read More
+        </a>
+      </div>
+      <div className={s.media_icons}>
+        {iconsData.map((item, index) => {
+          return (
+            <a key={item.title} className={s.link} href="icon_link">
+              <i className={s.fab}>{item.icon}</i>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default About;
