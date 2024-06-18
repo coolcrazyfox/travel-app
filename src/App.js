8861618169ce0e19cdab59d025246f0de3c400fb
Andrei Malets <coolviktori@yandex.by>
2024-06-18 22:27:28 +0300

    changes Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./pages/About";
+import Explore from "./pages/Explore";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
 // import s from "./styles/Header.module.css";
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="home" element={<Hero />} />
         <Route path="about" element={<About />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
