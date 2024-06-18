2ae2d34713fb8aef59eb524a0ffac10c13ded86c
Andrei Malets <coolviktori@yandex.by>
2024-06-18 22:13:48 +0300

    changes App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./pages/About";
 // import s from "./styles/Header.module.css";
 
 function App() {
