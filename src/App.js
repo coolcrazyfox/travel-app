0ba697489f7320998cfb756507c891fb1b508c23
Andrei Malets <coolviktori@yandex.by>
2024-07-07 21:08:42 +0300

    changes App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import Header from "./components/Header";
-import Hero from "./pages/Hero";
-import About from "./pages/About";
-import Explore from "./pages/Explore";
-import Gallery from "./pages/Gallery";
-import Contact from "./pages/Contact";
 import RouterRoot from "./routes/RouterRoot";
 
 function App() {
