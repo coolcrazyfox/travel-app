93b2a50766889bc675d382197ef15991a3d6d22f
Andrei Malets <coolviktori@yandex.by>
2024-07-07 20:23:24 +0300

    changes App

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import About from "./pages/About";
 import Explore from "./pages/Explore";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
-// import s from "./styles/Header.module.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="home" element={<Hero />} />
+        <Route path="*" element={<Hero />} />
         <Route path="about" element={<About />} />
         <Route path="explore" element={<Explore />} />
         <Route path="gallery" element={<Gallery />} />
